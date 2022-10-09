f1d1acfacc2c7593c6cffadaebf54995ccf510e5
Yqisme490 <80311931+Yqisme490@users.noreply.github.com>
2022-10-09 22:46:03 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,3 +1,9 @@
 var vg_1 = "homework3.vg.json";
+var vg_2 = "homework4.vg.json";
+
 vegaEmbed("#map", vg_1).then(function(result) {
 }).catch(console.error);
+
+vegaEmbed("#interact", vg_2).then(function(result) {
+}).catch(console.error);
+
